70e841c4646b45b6138ac54eb3e2e1e108140abe
Sourabh Cheedella <cheedella.sourabh@gmail.com>
2020-03-11 14:19:48 -0400

    prettify stagger-mode.el

--- elisp/stagger-mode.el
+++ elisp/stagger-mode.el
@@ -1,12 +1,18 @@
+;;; stagger-mode --- stagger the case in a buffer
 ;; -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;;; !Ref Spongebob meme
 (require 'cl-lib)
 
+;;; Code:
 (define-minor-mode stagger-mode
   "StAgGeR ThE CaSe iN ThE BuFfeR."
   :lighter " stagger"
   (add-to-list 'after-change-functions #'stagger-mode--do-it))
 
 (defun stagger-mode--do-it (beg end _)
+  "Inner function for actually performing staggers.  Start at (BEG) and end at (END)."
   (when stagger-mode
     (save-excursion
       (cl-loop for pos from beg below end
@@ -18,3 +24,4 @@
                          (insert-before-markers upcase))))))
 
 (provide 'stagger-mode)
+;;; stagger-mode ends here
